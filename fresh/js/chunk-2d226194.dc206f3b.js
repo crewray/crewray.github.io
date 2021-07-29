788ec3959164cb9290a66f3a8f1a8f15f53d30d5
lei <lei@qq.com>
2021-07-29 10:36:01 +0800

    add app

---
+++ fresh/js/chunk-2d226194.dc206f3b.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d226194"],{e6e2:function(e,n,t){"use strict";t.r(n);var s=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"main"},[t("router-view")],1)},u=[],a=t("2877"),c={},i=Object(a["a"])(c,s,u,!1,null,null,null);n["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d226194.dc206f3b.js.map
